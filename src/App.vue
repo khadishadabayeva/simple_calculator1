3c163b2c20bcdff89ebbb894a18310ef671e53bb
Khadisha97 <khadisha.dabayeva@gmail.comgit config --global user.name Github khadishadabayeva>
2020-11-20 10:22:03 +0100

    First programming task

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+Number 1: <input v-model.number="number1" type="text">
+Number 2: <input v-model.number="number2" type="text">
+<button v-on:click='add'>+</button>
+<button v-on:click='substract'>-</button>
+<button v-on:click='multiply'>*</button>
+<button v-on:click='divide'>/</button>
+Result: {{result}}
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return{
+      number1: 0,
+      number2: 0,
+      result: 0
+    }
+  },
+  methods: {
+    add () {
+      this.result = this.number1 + this.number2
+    },
+    substract () {
+      this.result = this.number1 - this.number2
+    },
+    multiply () {
+      this.result = this.number1 * this.number2
+    },
+    divide () {
+      this.result = this.number1 / this.number2
+    }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
